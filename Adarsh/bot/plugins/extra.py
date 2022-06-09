9c2bc7c8c7a72a97917d6e4b9bbce4dd45fbe8c1
JiC54lab <105932652+JiC54lab@users.noreply.github.com>
2022-06-09 21:33:15 +0200

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,7 +17,7 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I was Coded and being maintained By [JiC54](https://t.me/JiC54)",
+                    text="This Bot was Coded and being maintained By [JiC54](https://t.me/JiC54)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -30,6 +30,45 @@ async def maintainers(b,m):
                     disable_web_page_preview=True)
             
          
+        
+        @StreamBot.on_message(filters.command("changelog"))
+async def maintainers(b,m):
+    try:
+       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
+    except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**v8.8 Changelog**\n●Fixed link buttons in channels:\n There was an issue where the bot could not create valid links in channels, this has now been fixed\n
+                    ●Standard keyboard rearranged to suit small screens\n●Fixed streaming page for small screens\n●Added changelog command\n●All bugs in previous version fixed\n
+                    \n**v8.6 Changelog**\n
+                    •Superfast download and stream links. Download speeds up to 20mbps depending on your internet bandwidth.\n
+                    •No ads in generated links.\n
+                    •Superfast interface.\n
+                    •Along with the links that can be copied with a single tap you also get file information like name, size ,etc.\n
+                    •Support command. Get support from our team, and you can also report issues and bugs as well as sending feedback to the team.\n
+                    •Password Protection. Added login command. This feature has just been introduced and will be useful in future versions.\n
+                    •User Friendly Interface.\n
+                    •Ping check.\n
+                    •User Data Centre check.\n
+                    •Real time CPU, RAM, Internet usage check.\n
+                    •Channel supported. Just add bot Channel as Admin. If any new file comes in Channel it will edit it with stream and Download Link Button.\n
+                    •Supports audio streaming.\n
+                    •Enhanced Video streaming. Added new video player that supports many media formats and codecs. This new player also support pop up play, Fullscreen ability, download button and also allows the user to   customize settings via settings button. All these features on the video player.\n
+                    •Customizable streaming page. The ability to change day & night mode added.\n
+                    •Updated terms and conditions of use\n
+                    •Fixed all bugs in previous version.",
+                    
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("Terms & Conditions", url=f"bit.ly/3xgjGWf")
+                            ]
+                        ]
+                    ),
+                    parse_mode="markdown",
+                    disable_web_page_preview=True)
+        
+        
 @StreamBot.on_message(filters.command("follow") | filters.regex("follow❤️"))
 async def follow_user(b,m):
     try:
@@ -37,7 +76,7 @@ async def follow_user(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="<B>HERE'S THE FOLLOW LINK</B>",
+                    text="<b>HERE'S THE FOLLOW LINK</b>",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -48,8 +87,10 @@ async def follow_user(b,m):
                     ),
                     parse_mode="HTML",
                     disable_web_page_preview=True)
+            
+                     
         
-
+        
 @StreamBot.on_message(filters.command("dc") | filters.regex("DC"))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.dc_id)
@@ -63,7 +104,7 @@ async def start(bot, update):
     
 @StreamBot.on_message(filters.command("list"))
 async def list(l, m):
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4.`follow❤️` \n 5. `ping📡` \n 6. `status📊` \n 7. `DC` this tells your telegram dc \n 8. `maintainers😎` "
+    LIST_MSG = "**Hi! {} Here is a list of all my commands** \n \n **1.** `start⚡️` \n **2.** `help📚` \n **3.** `login🔑` \n **4.** `follow❤️` \n **5.** `ping📡` \n **6.** `status📊` check bot status \n **7.** `DC` this tells your telegram data center \n **8.** `maintainers😎` "
     await l.send_message(chat_id = m.chat.id,
         text = LIST_MSG.format(m.from_user.mention(style="md")),
         reply_to_message_id = m.message_id,
