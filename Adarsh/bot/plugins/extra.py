a7bbaea9f154daacda25af289767843381e276dc
JiC54 <98804496+JiC54@users.noreply.github.com>
2022-06-10 09:39:22 +0200

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -35,7 +35,7 @@ async def maintainers(b,m):
         @StreamBot.on_message(filters.command("changelog") | filters.regex("changelog❤️"))
 async def changelog(b,m):
     try:
-       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
+       await b.send_message(chat_id=m.chat.id,text="HELL",quote=True)
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
