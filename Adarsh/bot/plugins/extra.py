a95653241a0786aceb15f3664ad779b0cee4eeee
JiC54 <jic54studio@gmail.com>
2022-10-02 23:13:52 +0200

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -232,6 +232,29 @@ async def verified(v, m):
     await v.send_message(chat_id = m.chat.id,
         text = notverified)
 
+@StreamBot.on_message(filters.command("premium"))
+async def is_premium(p, m):
+    notpremium = "You are not a premium Telegram user"
+    premium = "You are a premium Telegram user"
+    if m.from_user.is_premium == True:
+        return  
+    await v.send_message(chat_id = m.chat.id,
+        text = premium)
+    if m.from_user.is_premium == False:
+        return
+    await v.send_message(chat_id = m.chat.id,
+        text = notpremium)
+
+@StreamBot.on_message(filters.command("feedback"))
+async def feedback(f, m):
+    feedback_text = """<b>How can we improve FilesToLinks so that it serves you the best?</b>
+    <i>Regarding anything, we want your opinion.</i>
+
+    Dear {},
+    We never stop striving to create FilesToLinks exactly what you require for your professional or domestic use. We need your input today to help us make FilesToLinks the best it can be.  Your feedback helps us decide which features to add and what enhancements should be made to our platform. To provide feedback, please click below."""
+    await f.send_message(chat_id = m.chat.id,
+        text = feedback_text.format(m.from_user.mention))
+
 @StreamBot.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "home":
