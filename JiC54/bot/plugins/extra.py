159c991fa09476fecdc0c43c7aa98a201e5219f1
JiC54 <jic54studio@gmail.com>
2022-10-15 01:12:12 +0200

    Update extra.py

--- JiC54/bot/plugins/extra.py
+++ JiC54/bot/plugins/extra.py
@@ -13,7 +13,7 @@ from pyrogram.handlers import MessageHandler
 from urllib.parse import quote
 
 BITLY_API = os.environ.get("BITLY_API", "8df1df8c23f719e5cf97788cc2d40321ea30092b")
-CUTTLY_API = os.environ.get("CUTTLY_API", "f64dffbde033b6c307387dd50b7c76e505f1c")
+CUTTLY_API = os.environ.get("CUTTLY_API", "8cb59cbecc2d349e4a0f31f05a8b020655b83")
 SHORTCM_API = os.environ.get("SHORTCM_API", "pk_...NIZv")
 GPLINKS_API = os.environ.get("GPLINKS_API", "3f84819e8fd9f234b0150fb6d39e7ab9b6bf370c")
 
@@ -337,3 +337,128 @@ async def cb_data(bot, update):
         await update.answer(text=hi, show_alert=True)
     else:
         await update.message.delete()
+@Client.on_message(filters.command(["short"]) & filters.regex(r'https?://[^\s]+'))
+async def reply_shortens(bot, update):
+    message = await update.reply_text(
+        text="`Analysing your link...`",
+        disable_web_page_preview=True,
+        quote=True
+    )
+    link = update.matches[0].group(0)
+    shorten_urls = await short(link)
+    await message.edit_text(
+        text=shorten_urls,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
+
+@Client.on_inline_query(filters.regex(r'https?://[^\s]+'))
+async def inline_short(bot, update):
+    link = update.matches[0].group(0),
+    shorten_urls = await short(link)
+    answers = [
+        InlineQueryResultArticle(
+            title="Short Links",
+            description=update.query,
+            input_message_content=InputTextMessageContent(
+                message_text=shorten_urls,
+                disable_web_page_preview=True
+            ),
+            reply_markup=reply_markup
+        )
+    ]
+    await bot.answer_inline_query(
+        inline_query_id=update.id,
+        results=answers
+    )
+
+async def short(link):
+    shorten_urls = "**--Shorted URLs--**\n"
+    
+    # Bit.ly shorten
+    if BITLY_API:
+        try:
+            s = Shortener(api_key=BITLY_API)
+            url = s.bitly.short(link)
+            shorten_urls += f"\n**Bit.ly :-** {url}"
+        except Exception as error:
+            print(f"Bit.ly error :- {error}")
+    
+    # Chilp.it shorten
+    try:
+        s = Shortener()
+        url = s.chilpit.short(link)
+        shorten_urls += f"\n**Chilp.it :-** {url}"
+    except Exception as error:
+        print(f"Chilp.it error :- {error}")
+    
+    # Clck.ru shorten
+    try:
+        s = Shortener()
+        url = s.clckru.short(link)
+        shorten_urls += f"\n**Clck.ru :-** {url}"
+    except Exception as error:
+        print(f"Click.ru error :- {error}")
+    
+    # Cutt.ly shorten
+    if CUTTLY_API:
+        try:
+            s = Shortener(api_key=CUTTLY_API)
+            url = s.cuttly.short(link)
+            shorten_urls += f"\n**Cutt.ly :-** {url}"
+        except Exception as error:
+            print(f"Cutt.ly error :- {error}")
+    
+    # Da.gd shorten
+    try:
+        s = Shortener()
+        url = s.dagd.short(link)
+        shorten_urls += f"\n**Da.gd :-** {url}"
+    except Exception as error:
+        print(f"Da.gd error :- {error}")
+    
+    
+    # Osdb.link shorten
+    try:
+        s = Shortener()
+        url = s.osdb.short(link)
+        shorten_urls += f"\n**Osdb.link :-** {url}"
+    except Exception as error:
+        print(f"Osdb.link error :- {error}")
+    
+    
+    # Short.cm shorten
+    if SHORTCM_API:
+        try:
+            s = Shortener(api_key=SHORTCM_API)
+            url = s.shortcm.short(link)
+            shorten_urls += f"\n**Shortcm : ** {url}"
+        except Exception as error:
+            print(f"Short.cm error :- {error}")
+    
+    # TinyURL.com shorten
+    try:
+        s = Shortener()
+        url = s.tinyurl.short(link)
+        shorten_urls += f"\n**TinyURL : ** {url}"
+    except Exception as error:
+        print(f"TinyURL.com error :- {error}")
+    
+    # GPLinks shorten
+    try:
+        api_url = "https://gplinks.in/api"
+        params = {'api': GPLINKS_API, 'url': link}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url, params=params, raise_for_status=True) as response:
+                data = await response.json()
+                url = data["shortenedUrl"]
+                shorten_urls += f"\n**GPLinks : ** {url}"
+    except Exception as error:
+        print(f"GPLink error :- {error}")
+    
+    # Send the text
+    try:
+        shorten_urls += "\n\nPlease note that this feature is for our donors, you may soon not be able to use it if you haven't made any donation, so please /donate to continue accessing this awesome feature."
+        return shorten_urls
+    except Exception as error:
+        return error
