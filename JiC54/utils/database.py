cab543b7171ed4f7d8083fcb00d2401d3ebbdf88
JiC54 <jic54studio@gmail.com>
2022-10-31 19:19:36 +0200

    Update database.py

--- JiC54/utils/database.py
+++ JiC54/utils/database.py
@@ -2,6 +2,11 @@
 import datetime
 import motor.motor_asyncio
 from JiC54.confing import get_int_key, get_str_key
+import asyncio
+import sys
+from motor import motor_asyncio
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = "mongodb+srv://jic54autofilter:jic54autofilter@cluster0.nbp6y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
