7b0487eb4c0a60f5c31d961c9564d9b9a06363c0
JiC54 <jic54studio@gmail.com>
2022-10-31 15:48:53 +0200

    Update __init__.py

--- JiC54/__init__.py
+++ JiC54/__init__.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from aiohttp import ClientSession
 from telethon.sessions import MemorySession
 from telethon import TelegramClient
+import logging
 import time
 StartTime = time.time()
 __version__ = 1.1
@@ -16,3 +17,12 @@ API_HASH = os.environ.get("API_HASH", None)
 
 
 telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("aiohttp").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+LOGGER = logging.getLogger('[JiC54]')
