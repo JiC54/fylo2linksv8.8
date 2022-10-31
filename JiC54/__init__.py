3e035172438fc5a1d451314bc32e2ab6e79030c9
JiC54 <jic54studio@gmail.com>
2022-10-31 15:06:14 +0200

    Update __init__.py

--- JiC54/__init__.py
+++ JiC54/__init__.py
@@ -1,7 +1,17 @@
 # JiC54
 
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
 from aiohttp import ClientSession
+from telethon.sessions import MemorySession
 import time
 StartTime = time.time()
 __version__ = 1.1
 aiohttpsession = ClientSession()
+
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+
+
+telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
