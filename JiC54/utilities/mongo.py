53ea94ab530d8fa9e11a5d5d59df99cb5703ec6c
JiC54 <jic54studio@gmail.com>
2022-10-31 15:31:59 +0200

    Update mongo.py

--- JiC54/utilities/mongo.py
+++ JiC54/utilities/mongo.py
@@ -4,13 +4,16 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from JiC54.vars import DATABASE_URL as MONGO_DB_URI
+from JiC54.vars import Var
+
+
 
 from JiC54.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "SiestaRobot"
+MONGO_DB_URI = Database(Var.DATABASE_URL)
+MONGO_DB = Database(Var.name)
 
 
 client = MongoClient()
