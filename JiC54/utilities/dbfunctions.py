54b8b80ebb3f2827a49076262c629b2f0827b3d2
JiC54 <jic54studio@gmail.com>
2022-10-31 16:32:21 +0200

    Update dbfunctions.py

--- JiC54/utilities/dbfunctions.py
+++ JiC54/utilities/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from JiC54.utilities.mongo import db
+from JiC54.utils.database import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
