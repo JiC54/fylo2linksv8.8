093aac65b0c93a269ee175f97daed1d25c0a452a
JiC54 <jic54studio@gmail.com>
2022-11-01 07:58:13 +0200

    Create webscreenshot.py

---
+++ JiC54/bot/plugins/webscreenshot.py
@@ -0,0 +1,68 @@
+"""
+JiC54
+"""
+from pyrogram import filters
+from pyrogram.types import Message
+from JiC54.bot import StreamBot
+import sys
+import logging
+import traceback
+from functools import wraps
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+
+
+LOGGER = logging.getLogger('JiC54')
+
+__mod_name__ = "Webshot​"
+
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    "".join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await app.send_message(LOGGER, x)
+            raise err
+
+    return capture
+
+@StreamBot.on_message(filters.command("webss"))
+@capture_err
+async def take_ss(_, message: Message):
+    try:
+        if len(message.command) != 2:
+            return await message.reply_text(
+                "Give A Url To Fetch Screenshot."
+            )
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("**Taking Screenshot**")
+        await m.edit("**Uploading**")
+        try:
+            await message.reply_photo(
+                photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
+            )
+        except TypeError:
+            return await m.edit("No Such Website.")
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
