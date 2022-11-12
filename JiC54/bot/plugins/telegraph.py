3d16e07d1d3e99d7cfccb28efd9e464cd5012b99
JiC54 <jic54studio@gmail.com>
2022-11-13 00:20:59 +0200

    Update telegraph.py

--- JiC54/bot/plugins/telegraph.py
+++ JiC54/bot/plugins/telegraph.py
@@ -5,6 +5,7 @@ from JiC54.bot import StreamBot
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from telegraph import upload_file
+from urllib.parse import quote
 
 @StreamBot.on_message(filters.command(["tgmedia", "tgraph", "telegraph"]))
 async def telegraph(client, message):
@@ -54,3 +55,23 @@ async def telegraph(client, message):
     )
     finally:
         os.remove(download_location)
+
+def share_link(text: str) -> str:
+    return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
+
+@StreamBot.on_message(filters.command(["sharetext", "st", "stxt", "shtxt", "shtext"]))
+async def share_text(client, message):
+    reply = message.reply_to_message
+    reply_id = message.reply_to_Message.id if message.reply_to_message else Message.id
+    input_split = message.text.split(None, 1)
+    if len(input_split) == 2:
+        input_text = input_split[1]
+    elif reply and (reply.text or reply.caption):
+        input_text = reply.text or reply.caption
+    else:
+        await message.reply_text(
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",
+            reply_to_message_id=reply_id
+        )
+        return
+    await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
